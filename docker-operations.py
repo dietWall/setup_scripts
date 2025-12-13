3aae43cae3c56589381dfdb8d111ccfd03fe5616
dietWall <dietrich7@gmx.de>
2025-12-13 17:26:30 +0100

    .env now available everywhere .env is not loaded on host environment
    Signed-off-by: dietWall <dietrich7@gmx.de>

--- docker-operations.py
+++ docker-operations.py
@@ -11,23 +11,22 @@ def get_repo_root():
     repo_root = subprocess.run(["git", "rev-parse", "--show-toplevel"],capture_output=True)
     return repo_root.stdout.strip().decode('utf-8')
 
-def load_env() -> bool:
+def load_env() -> dict:
+    '''
+    loads .env file if it exists in repo root
+    :return: dictionary of key-value pairs or empty dict if no .env file found.
+    :rtype: dict[Any, Any]
+    '''
     import os
     if os.path.exists('.env'):
-        from dotenv import load_dotenv
-        return load_dotenv('.env')
-    return False
+        from dotenv import dotenv_values
+        config = dotenv_values(".env")
+        return config
+    return {}
 
 def build_image() -> bool:
     print(f"building {test_image_tag}")
-    build_args = None
-
-    if load_env() == False:
-        print("No .env file found or could not be loaded, proceeding without it.")
-    else:
-        import os
-        build_args = {"PASSWORD": os.environ['PASSWORD']}
-
+    build_args =  load_env()
     client = docker.from_env()
     client.images.build(path=".", tag=test_image_tag, buildargs=build_args)
     print(f"Successfully built image: {test_image_tag}")
@@ -39,7 +38,7 @@ def run_container(image_tag: str) -> Container:
     mounts=[docker.types.Mount(target="/home/appuser/code/", source=f"{get_repo_root()}", type="bind", read_only=False)]
     client = docker.from_env()
     
-    container = client.containers.run(image_tag, detach=True, tty=True, mounts=mounts)
+    container = client.containers.run(image_tag, detach=True, tty=True, mounts=mounts, environment=load_env())
     print(f"Container {container.id} is running.")
     print(f"enter with: ")
     print(f"docker exec -it {container.name} bash")
