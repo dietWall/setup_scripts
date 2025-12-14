37ea9faccbb6df4e0e153f0bff0e6dd8c5a4e5c6
dietWall <dietrich7@gmx.de>
2025-12-14 09:06:18 +0100

    finished tests for the beginning
    Signed-off-by: dietWall <dietrich7@gmx.de>

--- tests/test-ssh-keys.py
+++ tests/test-ssh-keys.py
@@ -19,18 +19,9 @@ def test_show_code_directory(client_container):
     for line in exec_result.output[0].decode('utf-8').splitlines():
         print(f"{line}")
 
-
-
 @pytest.mark.parametrize("key_type", ["rsa", "dsa", "ecdsa", "ed25519"])
 def test_ssh_keys_generation(client_container, key_type):
     print("") #newline 
-
-    cmd = "ls -la /home/appuser/code/ssh/"
-    exec_result = client_container.exec_run(cmd=cmd, demux=True)
-
-    for line in exec_result.output[0].decode('utf-8').splitlines():
-        print(f"{line}")
-
 
     key_path = f"/home/appuser/keys/"
     cmd=f"/home/appuser/code/ssh/ssh-keys.py --generate {key_type} --key-path={key_path}"
@@ -53,6 +44,24 @@ def test_ssh_keys_generation(client_container, key_type):
     if exec_result.output[0] is not None:
         for line in exec_result.output[0].decode('utf-8').splitlines():
             print(f"{line}")
-            
-        
+
+@pytest.mark.parametrize("key_type", ["rsa", "dsa", "ecdsa", "ed25519"])
+def test_ssh_keys_deploy(client_container, server_container, key_type):
+    print("") #newline 
+
+    print(f"testing deployment of {key_type} key to {server_container.name}")
     
+    cmd=f'''/bin/bash -c \"source /home/appuser/ssh-venv/bin/activate && /home/appuser/code/ssh/ssh-keys.py --generate {key_type} --deploy appuser@ssh-server --password_type dotenv\"'''
+
+    print(f"running:")
+    print(f"{cmd}")
+
+    exec_result = client_container.exec_run(cmd=cmd, demux=True)
+    print(f"exit code: {exec_result.exit_code}")
+    
+    for line in exec_result.output[0].decode('utf-8').splitlines():
+        print(f"{line}")
+
+    assert exec_result.exit_code == 0, "Key deployment failed"
+    # it would be good to test the connection without password, which is actually the final goal
+    # but that requires more setup
