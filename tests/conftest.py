2c5d0df08053498fcca6fd4cfa7df5cedb27f582
dietWall <dietrich7@gmx.de>
2026-02-13 11:54:06 +0100

    added tests for repo_helper
    Signed-off-by: dietWall <dietrich7@gmx.de>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import subprocess
 
 def create_empty_env_file(repo_root):
     env_path = f"{repo_root}/.env"
@@ -65,3 +66,39 @@ def client_container(containers):
 @pytest.fixture(scope="session", autouse=True)
 def server_container(containers):
     return server(containers)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def log_dir():
+    dir = "test_log"
+    command = f"mkdir -p {dir}"
+    result = subprocess.run(command, shell=True)
+    return dir
+
+@pytest.fixture(scope="session", autouse=True)
+def build_repo_helper(repo_root):
+    cmd = "python3 -m build ."
+    subprocess.run(args=cmd, shell=True, cwd=repo_root)
+
+@pytest.fixture(scope="session", autouse=True)
+def repo_helper(repo_root, build_repo_helper):
+    cmd = "pip install ."
+    subprocess.run(args=cmd, shell=True, cwd=repo_root)
+
+containers_to_stop = []
+
+@pytest.fixture(scope="function")
+def cleanup(container_name: str):
+    #this takes all container names, that has been started during a test
+    # and stops it afterwards, otherwise the test will fail in the next run
+    print(f"started container: {container_name}")
+    containers_to_stop.append(container_name)
+    yield
+    for c in containers_to_stop:
+        subprocess.run(f"docker stop {c}", shell=True)
+        subprocess.run(f"docker rm {c}", shell=True)
+
+
+
+
+    
